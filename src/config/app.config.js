ee9e825450c951a38b8afbef99282828ab8c7390
TriTruong666 <tritruonghoang3@gmail.com>
2026-05-28 16:16:39 +0700

    feat: initialize application configuration module with environment and database loading support

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -40,7 +40,7 @@ const appConfig = {
     name: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    reset: false,
+    reset: true,
     force_reset: false,
     protectedTables: [
       "users",
