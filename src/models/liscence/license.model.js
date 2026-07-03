a17c01a915f408b97d22cf53d84638c652166eb3
TriTruong666 <tritruonghoang3@gmail.com>
2026-07-03 12:05:57 +0700

    feat: initialize application configuration module and define License data model

--- src/models/liscence/license.model.js
+++ src/models/liscence/license.model.js
@@ -9,6 +9,13 @@ const License = sequelize.define(
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
     },
+    licenseKey: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      field: "license_key",
+      comment: "Khoá định danh duy nhất do hệ thống cấp cho khách hàng",
+    },
     customerName: {
       type: DataTypes.STRING(255),
       allowNull: false,
@@ -34,6 +41,11 @@ const License = sequelize.define(
     tableName: "licenses",
     timestamps: true,
     indexes: [
+      {
+        name: "licenses_license_key_unique",
+        unique: true,
+        fields: ["license_key"],
+      },
       { fields: ["customer_email"] },
       { fields: ["customer_phone"] },
     ],
